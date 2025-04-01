0e760c64d92e258a26bb3b0cc831c609e949a5db
Tsegaye Habenom Yeabiyo <60841951+tse-code@users.noreply.github.com>
2025-04-01 15:22:51 -0500

    Update Data Cleaning Portfolio Project Queries.sql

--- Data Cleaning Portfolio Project Queries.sql
+++ Data Cleaning Portfolio Project Queries.sql
@@ -69,10 +69,93 @@ FROM layoffs_staging2
 WHERE row_num>1;
 
 SELECT *
-FROM layoffs_staging2
+FROM layoffs_staging2;
+
+
 
 
 # standardizing data
+select company, trim(company)
+From layoffs_staging2;
 
-SELECT company,Trim(company)
-From layoffs_staging2
+update layoffs_staging2
+set company=trim(company);
+
+select company
+from layoffs_staging2;
+
+select *
+from layoffs_staging2
+where industry like 'Crypto%';
+
+update layoffs_staging2
+set industry = 'Crypto'
+where industry like 'Crypto%';
+
+select distinct location
+from layoffs_staging2
+order by 1;
+
+select distinct country
+from layoffs_staging2
+order by 1;
+
+update layoffs_staging2
+set country ='United States'
+where country like 'United States%';
+
+select date,
+str_to_date(date , '%m/%d/%Y')
+from layoffs_staging2;
+
+update layoffs_staging2
+set date = str_to_date(date , '%m/%d/%Y');
+
+alter table layoffs_staging2
+modify column date DATE;
+
+select *
+from layoffs_staging2
+where total_laid_off is null
+and percentage_laid_off is null;
+
+update layoffs_staging2
+set industry=null
+where industry =''; 
+
+select *
+from layoffs_staging2 t1
+join layoffs_staging2 t2
+	on t1.company = t2.company
+    
+where (t1.industry is null  or t1.industry= '')
+and t2. industry is not null;
+
+update layoffs_staging2 t1
+join layoffs_staging2 t2
+	on t1.company = t2.company
+set t1.industry = t2.industry
+where t1.industry is null
+and t2. industry is not null;
+
+select * 
+from layoffs_staging2
+where company ='Airbnb';
+
+select *
+from layoffs_staging2;
+
+delete
+from layoffs_staging2
+where total_laid_off is null
+and percentage_laid_off is null;
+
+select *
+from layoffs_staging2
+where total_laid_off is null
+and percentage_laid_off is null;
+
+alter table layoffs_staging2
+drop column row_num;
+
+
